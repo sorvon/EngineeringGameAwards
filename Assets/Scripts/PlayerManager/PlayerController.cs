9f5e5189352d5f90b29deccafc20628c9a185717
sorvon <sorvon@qq.com>
2022-12-23 23:57:40 +0800

    导入测试资源，搭建了一个简单的场景

---
+++ Assets/Scripts/PlayerManager/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float velocity = 10;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = velocity * new Vector2(
+            Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+}
