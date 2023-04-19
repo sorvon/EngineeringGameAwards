0045884d19e23ddfb832f08eb0e61b673019e7e2
sorvon <sorvon@qq.com>
2023-04-19 14:52:38 +0800

    修改了目录结构

--- Assets/Scripts/Dig/StateManager.cs
+++ Assets/Scripts/Dig/StateManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LifeManager : MonoBehaviour
+public class StateManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
