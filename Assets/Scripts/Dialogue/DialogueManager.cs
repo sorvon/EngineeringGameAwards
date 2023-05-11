ab11be7e68c7a0ce2e5da62a9f88322aa3c3150e
dengzi566 <116444638+dengzi566@users.noreply.github.com>
2023-05-11 20:56:47 +0800

    文本框架基本完成

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -10,16 +10,38 @@ public class DialogueManager : MonoBehaviour
 
     [Header("Dialogue UI")]
     [SerializeField] private GameObject dialoguePanel;
+
     [SerializeField] private TextMeshProUGUI dialogueText;
 
     [Header("Choices UI")]
+
     [SerializeField] private GameObject[] choices;
     private TextMeshProUGUI[] choicesText;
 
     private Story currentStory;
-    public bool dialogueIsPlaying { get; private set; }
+    public bool dialogueIsPlaying; //{ get; private set; }
 
     private static DialogueManager instance;
+    [SerializeField] private TextMeshProUGUI displayNameText;
+  /*  [SerializeField] private Animator portraitAnimator;
+    private Animator layoutAnimator;*/
+
+    private const string SPEAKER_TAG = "speaker";
+    private const string PORTRAIT_TAG = "portrait";
+    private const string LAYOUT_TAG = "layout";
+    //private const string AUDIO_TAG = "audio";
+
+    public SpriteRenderer speakerLeft;
+    public SpriteRenderer speakerRight;
+    private SpriteRenderer speakerNow;
+    private Sprite crtNow;
+    public Sprite crtA;
+    public Sprite crtB;
+    public Sprite crtC;
+    public RectTransform speakerFrame;
+    public RectTransform positionLeft;
+    public RectTransform positionRight;
+    public GameObject trigger;
 
     private void Awake()
     {
@@ -39,7 +61,7 @@ public class DialogueManager : MonoBehaviour
     {
         dialogueIsPlaying = false;
         dialoguePanel.SetActive(false);
-
+        //dialogueChoices.SetActive(false);
         // get all of the choices text 
         choicesText = new TextMeshProUGUI[choices.Length];
         int index = 0;
@@ -52,6 +74,7 @@ public class DialogueManager : MonoBehaviour
 
     private void Update()
     {
+        Debug.Log(dialogueIsPlaying);
         // return right away if dialogue isn't playing
         if (!dialogueIsPlaying)
         {
@@ -77,13 +100,15 @@ public class DialogueManager : MonoBehaviour
         ContinueStory();
     }
 
-    private IEnumerator ExitDialogueMode()
+    public void ExitDialogueMode()
     {
-        yield return new WaitForSeconds(0.2f);
-
+        //yield return new WaitForSeconds(0.2f);
+        Debug.Log("退出对话");
         dialogueIsPlaying = false;
         dialoguePanel.SetActive(false);
         dialogueText.text = "";
+        trigger.SetActive(false);
+        return;
     }
 
     private void ContinueStory()
@@ -94,12 +119,13 @@ public class DialogueManager : MonoBehaviour
             dialogueText.text = currentStory.Continue();
             // display choices, if any, for this dialogue line
             DisplayChoices();
+            HandleTags(currentStory.currentTags);
             Debug.Log("继续对话");
         }
         else
         {
-            Debug.Log("退出对话");
-            StartCoroutine(ExitDialogueMode());
+            
+            ExitDialogueMode();
         }
     }
 
@@ -118,6 +144,7 @@ public class DialogueManager : MonoBehaviour
         // enable and initialize the choices up to the amount of choices for this line of dialogue
         foreach (Choice choice in currentChoices)
         {
+            dialogueText.text = "";
             choices[index].gameObject.SetActive(true);
             choicesText[index].text = choice.text;
             index++;
@@ -149,4 +176,76 @@ public class DialogueManager : MonoBehaviour
         ContinueStory();
     }
 
+    private void HandleTags(List<string> currentTags)
+    {
+        // loop through each tag and handle it accordingly
+        foreach (string tag in currentTags)
+        {
+            // parse the tag
+            string[] splitTag = tag.Split(':');
+            if (splitTag.Length != 2)
+            {
+                Debug.LogError("Tag could not be appropriately parsed: " + tag);
+            }
+            string tagKey = splitTag[0].Trim();
+            string tagValue = splitTag[1].Trim();
+
+            // handle the tag
+            switch (tagKey)
+            {
+                case SPEAKER_TAG:
+                    Debug.Log(tagValue);
+                    displayNameText.text = tagValue;
+                    CharacterConvert(tagValue);
+                    break;
+              /*  case PORTRAIT_TAG:
+                    portraitAnimator.Play(tagValue);
+                    break;*/
+                case LAYOUT_TAG:
+                    Debug.Log(tagValue);
+                    if (tagValue == "left")
+                    {
+                        speakerLeft.color = Color.white;
+                        speakerRight.color = Color.gray;
+                        speakerNow = speakerLeft;
+                        speakerFrame.position = positionLeft.position;
+                        speakerLeft.sprite = crtNow;
+                    }
+                    else
+                    {
+                        speakerLeft.color = Color.gray;
+                        speakerRight.color = Color.white;
+                        speakerNow = speakerRight;
+                        speakerFrame.position = positionRight.position;
+                        speakerRight.sprite = crtNow;
+                    }
+                    //layoutAnimator.Play(tagValue);
+                    break;
+/*                case AUDIO_TAG:
+                    SetCurrentAudioInfo(tagValue);
+                    break;*/
+                default:
+                    Debug.LogWarning("Tag came in but is not currently being handled: " + tag);
+                    break;
+            }
+        }
+    }
+    void CharacterConvert(string tagValue)
+    {
+        switch (tagValue)
+        {
+            case "A":
+                crtNow = crtA;
+                break;
+            case "B":
+                crtNow = crtB;
+                break;
+            case "C":
+                crtNow = crtC;
+                break;
+            default:
+                Debug.LogWarning("tag:speaker出现问题");
+                break;
+        }
+    }
 }
--- Assets/Scripts/Quench/PlayerQuench.cs
+++ Assets/Scripts/Quench/PlayerQuench.cs
@@ -236,17 +236,5 @@ public class PlayerQuench : MonoBehaviour
         mySequence.Append(move2);
         mySequence.Join(alpha2);
     }
-    /*public void PositionConvert()
-    {
-        // 将obj1的世界坐标转换为屏幕坐标
-        Vector3 screenPos = Camera.main.WorldToScreenPoint(obj1.transform.position);
-
-        // 将屏幕坐标转换为ui坐标
-        Vector2 localPos;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(obj2, screenPos, null, out localPos);
-
-        // 将ui坐标赋值给obj2的anchoredPosition3D属性
-        scorechangedown.transform.position = localPos;
-    }*/
 
 }
