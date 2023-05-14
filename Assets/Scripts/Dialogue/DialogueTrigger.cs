1feb93fd084b90705fbb1ff5049e6266e09f8427
sorvon <sorvon@qq.com>
2023-05-14 18:17:26 +0800

    base对话实现

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class DialogueTrigger : MonoBehaviour
+public class DialogueTrigger : MonoBehaviour, IDataPersistence
 {
     [Header("Ink JSON")]
     [SerializeField] private TextAsset inkJSON;
+    GameData gameData;
 
     //private bool toEnterDialogue;
-  /*  void Awake()
-    {
-        toEnterDialogue = false;
-    }*/
+    /*  void Awake()
+      {
+          toEnterDialogue = false;
+      }*/
 
     // Update is called once per frame
     //void Update()
@@ -21,6 +23,52 @@ public class DialogueTrigger : MonoBehaviour
 
     //    }
     //}
+    private void Start()
+    {
+        if (DialogueManager.GetInstance().dialogueIsPlaying) return;
+        string sceneName = SceneManager.GetActiveScene().name;
+        switch (sceneName)
+        {
+            case "Base":
+                if (gameData.baseDialog)
+                {
+                    DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+                    gameData.baseDialog = false;
+                }
+                break;
+            case "Steel":
+                if (gameData.steelDialog)
+                {
+                    DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+                    gameData.steelDialog = false;
+                }
+                break;
+            case "SteelRolling":
+                if (gameData.steelRollingDialog)
+                {
+                    DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+                    gameData.steelRollingDialog = false;
+                }
+                break;
+            case "Quench DYH":
+                if (gameData.quenchDialog)
+                {
+                    DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+                    gameData.quenchDialog = false;
+                }
+                break;
+            case "Dig":
+                //if (gameData.baseDialog)
+                //{
+                //    DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+                //    gameData.baseDialog = false;
+                //}
+                break;
+            default:
+                break;
+        }
+        DataPersistenceManager.instance.SaveGame();
+    }
 
     public void EnterDialogue()
     {
@@ -30,5 +78,15 @@ public class DialogueTrigger : MonoBehaviour
         }
         
         //toEnterDialogue = true;
+    }
+
+    void IDataPersistence.LoadData(GameData gameData)
+    {
+        this.gameData = gameData;
+    }
+
+    void IDataPersistence.SaveData(GameData gameData)
+    {
+        gameData = this.gameData;
     }
 }
