42868248d70220531b5139b804e141ec8eb7111b
sorvon <sorvon@qq.com>
2023-09-02 15:07:28 +0800

    完成勘探目标点的随机生成

--- Assets/Scripts/Dig/CrosshairController.cs
+++ Assets/Scripts/Dig/CrosshairController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CrosshairController : MonoBehaviour
 {
@@ -8,27 +9,47 @@ public class CrosshairController : MonoBehaviour
     [SerializeField] float moveSpeed = 100;
     [SerializeField] GameObject beacon;
     [SerializeField] Transform dest;
+    [SerializeField] GameObject successMenu;
+    [SerializeField] SpriteRenderer rulerY;
+    [SerializeField] SpriteRenderer rulerX;
+    [SerializeField] float materialOffsetK = 1;
+    [SerializeField] float randomRMin = 1;
+    [SerializeField] float randomRMax = 3;
 
     [Header("Debug")]
     [SerializeField] List<GameObject> beaconList;
     [SerializeField] int beaconIndex = 0;
-    [SerializeField] Rigidbody2D rb;
-    [SerializeField] DigDialogManager digitalDialogManager;
+    Vector3 origin;
+    Image crossHairImage;
+    Rigidbody2D rb;
+    DigDialogManager digitalDialogManager;
     // Update is called once per frame
 
     private void Awake()
     {
+        
         rb = GetComponent<Rigidbody2D>();
+        crossHairImage = rb.GetComponent<Image>();
         digitalDialogManager = GetComponent<DigDialogManager>();
         beaconIndex = 0;
+    }
+    private void Start()
+    {
+        origin = transform.position;
+        dest.position = dest.position + Random.Range(randomRMin, randomRMax) * (Quaternion.Euler(0, 0, Random.Range(0, 360)) * Vector3.right);
     }
     void FixedUpdate()
     {
         float axix_v = Input.GetAxis("Vertical") * moveSpeed;
         float axix_h = Input.GetAxis("Horizontal") * moveSpeed;
         rb.velocity = new Vector3(axix_h, axix_v, 0);
+        var offset = transform.position - origin;
+
+        rulerX.material.mainTextureOffset = new Vector2(offset.x * materialOffsetK, 0);
+        rulerY.material.mainTextureOffset = new Vector2(0, offset.y * materialOffsetK);
+
         //transform.position += new Vector3(axix_h, axix_v, 0) * Time.deltaTime * moveSpeed;
-        
+
     }
 
     private void Update()
@@ -37,6 +58,14 @@ public class CrosshairController : MonoBehaviour
         {
             var bPos = transform.position;
             bPos.z = 0;
+            foreach (var b in beaconList)
+            {
+                if ((bPos - b.transform.position).magnitude < 0.5f)
+                {
+                    StartCoroutine(ChangeImageColor());
+                    return;
+                }
+            }
             if (beaconList.Count < 3)
             {
                 beaconList.Add(GameObject.Instantiate(beacon, bPos, transform.rotation));
@@ -62,9 +91,17 @@ public class CrosshairController : MonoBehaviour
                 }
                 if(less_1_cnt >= 2)
                 {
-                    digitalDialogManager.ToDig();
+                    Time.timeScale = 0;
+                    successMenu.SetActive(true);
                 }
             }
         }
+    }
+
+    IEnumerator ChangeImageColor()
+    {
+        crossHairImage.color = Color.red;
+        yield return new WaitForSeconds(0.5f);
+        crossHairImage.color = Color.green;
     }
 }
