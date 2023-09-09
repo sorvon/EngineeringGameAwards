a1ee32f2ffb20695bdd7382df178912a1c2512c0
sorvon <sorvon@qq.com>
2023-09-09 20:06:34 +0800

    修复图层bug

---
+++ Assets/Scripts/UIManager/EndingTrigger.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class EndingTrigger : MonoBehaviour, IDataPersistence
+{
+    static public EndingTrigger instance;
+    GameData gameData;
+    void IDataPersistence.LoadData(GameData gameData)
+    {
+        this.gameData = gameData;
+    }
+
+    void IDataPersistence.SaveData(GameData gameData)
+    {
+        gameData = this.gameData;
+    }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+        var swords = gameData.swords;
+        bool isEnd = true;
+        foreach (var sword in swords)
+        {
+            if (!sword)
+            {
+                isEnd = false;
+            }
+        }
+        //isEnd = true;
+        if (isEnd)
+        {
+            if (TryGetComponent<Animator>(out var animator))
+            {
+                animator.enabled = true;
+                var text = GetComponentInChildren<TextMeshProUGUI>(true);
+                if (text != null)
+                {
+                    text.outlineWidth = 0.158f;
+                }
+            }
+        }
+    }
+}
